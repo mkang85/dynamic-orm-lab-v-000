dba194370b09d2c5b29cc6f5302e95f7f808d89e
Michael Kang <michaelkang85@gmail.com>
2018-04-11 23:36:41 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,5 +8,15 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = PRAGMA table_info("#{table_name}")
+    table_info = DB[:conn].execute(sql)
+    col_names = []
+    table_info.each do |col|
+      col_names << col["name"]
+    end 
+    col_names.compact
+  end 
 
 end
